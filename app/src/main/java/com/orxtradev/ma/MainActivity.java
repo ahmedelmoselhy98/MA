0f96f258138ddcedb87140e9c909f41cd5d5e7e2
ahmed elmoselhy <ahmedelmoselhy98@gmail.com>
2019-01-05 18:13:56 +0200

    Initial commit

---
+++ app/src/main/java/com/orxtradev/ma/MainActivity.java
@@ -0,0 +1,15 @@
+package com.orxtradev.ma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button ahmed, mahmoud;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
